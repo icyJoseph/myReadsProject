a4cc965ccb4dc7d2c234987a4222fe30aadc9917
joseph_ctu <sephxd1234@gmail.com>
2017-10-29 21:22:15 +0100

    feat: BookControl.js displays default value and allows user to update it
    This component uses a book object, the currentshelf and moveShelf to
    display the correct default value and to allow the user to change
    the shelf of the given book.

--- src/BookControl.js
+++ src/BookControl.js
@@ -1,7 +1,7 @@
 import React from "react";
 import * as typeOfShelf from "./constants";
 
-const Selector = ({ props, id, currentShelf, moveShelf }) => {
+const Selector = ({ props, book, currentShelf, moveShelf }) => {
   const shelves = Object.keys(typeOfShelf);
   const options = shelves.map((shelf, i) => (
     <option key={i} value={shelf}>
@@ -9,7 +9,7 @@ const Selector = ({ props, id, currentShelf, moveShelf }) => {
     </option>
   ));
   return (
-    <select defaultValue={currentShelf} onChange={ (e) => moveShelf(id, e.target.value)}>
+    <select defaultValue={currentShelf} onChange={ (e) => moveShelf(book, e.target.value)}>
       <option value="none" disabled>
         Move to...
       </option>
