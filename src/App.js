5abd5df59485fb8b5ef2c16ca0bc39ce344c13ce
joseph_ctu <sephxd1234@gmail.com>
2017-10-29 21:16:08 +0100

    feat: App state manager and supporting methods.
    The App now fetches state from backend server after it mounts.
    It also has helper methods to fetch state, updateshelf, search a query,
    and clear a query.
    
    As a result of these additions, the search route now takes props.

--- src/App.js
+++ src/App.js
@@ -1,90 +1,95 @@
 import React, { Component } from "react";
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from "./BooksAPI";
 import { Route } from "react-router-dom";
 import SearchBooks from "./SearchBooks";
 import Shelves from "./Shelves";
-
+import * as typeOfShelf from "./constants";
 import "./App.css";
 
 class BooksApp extends Component {
   state = {
-    books: [
-      {
-        id: 0,
-        imgURL:
-          "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
-        title: "To Kill a Mockingbird",
-        author: "Harper Lee",
-        shelf: "currentlyReading"
-      },
-      {
-        id: 1,
-        imgURL:
-          "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
-        title: "Ender's Game",
-        author: "Orson Scott Card",
-        shelf: "currentlyReading"
-      },
-      {
-        id: 2,
-        imgURL:
-          "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
-        title: "1776",
-        author: "David McCullough",
-        shelf: "wantToRead"
-      },
-      {
-        id: 3,
-        imgURL:
-          "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api",
-        title: "Harry Potter and the Sorcerer's Stone",
-        author: "J.K. Rowling",
-        shelf: "wantToRead"
-      },
-      {
-        id: 4,
-        imgURL:
-          "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api",
-        title: "The Hobbit",
-        author: "J.R.R. Tolkien",
-        shelf: "read"
-      },
-      {
-        id: 5,
-        imgURL:
-          "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api",
-        title: "Oh, the places You'll Go!",
-        author: "Seuss",
-        shelf: "read"
-      },
-      {
-        id: 6,
-        imgURL:
-          "http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api",
-        title: "The Adventures of Tom Sawyer",
-        author: "Mark Twain",
-        shelf: "read"
-      }
-    ]
+    books: [],
+    response: [],
+    error: ""
   };
 
-  moveShelf = (id, newShelf) => {
-    this.setState(prevState => ({
-      books: prevState.books.map(
-        book => (book.id === id ? {...book, shelf: newShelf} : book)
+  componentDidMount() {
+    BooksAPI.getAll().then(books => this.setState({ books }));
+  }
+
+  moveShelf = (book, newShelf) => {
+    BooksAPI.update(book, newShelf)
+      .then(resp =>
+        BooksAPI.get(book.id).then(updatedBook => {
+          const [BookInState] = this.state.books.filter(
+            book => book.id === updatedBook.id
+          );
+          if (BookInState) {
+            this.setState(prevState => ({
+              books:
+                updatedBook.shelf === typeOfShelf.none
+                  ? prevState.books.filter(book => book.id !== BookInState.id)
+                  : prevState.books.map(
+                      book => (book.id === updatedBook.id ? updatedBook : book)
+                    )
+            }));
+          } else {
+            this.setState(prevState => ({
+              books: prevState.books.concat(updatedBook)
+            }));
+          }
+        })
       )
-    }));
+      .catch(err => console.log(err));
+  };
+
+  search = query => {
+    BooksAPI.search(query, 1)
+      .then(books => {
+        const { error } = books;
+        if (error) {
+          this.setState({ error });
+        } else {
+          this.setState(prevState => ({
+            response: books.map(book => {
+              const [booksInShelf] = prevState.books.filter(
+                localStateBooks => book.id === localStateBooks.id
+              );
+              return booksInShelf
+                ? booksInShelf
+                : { ...book, shelf: typeOfShelf.none };
+            }),
+            error: ""
+          }));
+        }
+      })
+      .catch(err => console.log(err));
+  };
+
+  clearQuery = () => {
+    this.setState({ response: [] });
   };
 
   render() {
-    const { books } = this.state;
+    const { books, response, error } = this.state;
     return (
       <div className="app">
-        <Route path="/search" component={SearchBooks} />
         <Route
           exact
           path="/"
           render={() => <Shelves books={books} moveShelf={this.moveShelf} />}
+        />
+        <Route
+          path="/search"
+          render={() => (
+            <SearchBooks
+              response={response}
+              error={error}
+              search={this.search}
+              clearQuery={this.clearQuery}
+              moveShelf={this.moveShelf}
+            />
+          )}
         />
       </div>
     );
