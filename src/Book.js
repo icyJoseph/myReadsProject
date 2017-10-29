b96ce24db8819e5fd9e732a0c8abed3e235c8399
joseph_ctu <sephxd1234@gmail.com>
2017-10-29 21:19:44 +0100

    feat: Book.js handles array of authors
    The Book.js component is now capable of handling author information
    as an array. It also handles Books where the author is unknown.

--- src/Book.js
+++ src/Book.js
@@ -3,23 +3,40 @@ import Selector from "./BookControl";
 
 class Book extends Component {
   render() {
-    const { book: { id, title, author, imgURL, shelf }, moveShelf } = this.props;
+    const {
+      book: { title, authors, imageLinks, shelf },
+      moveShelf
+    } = this.props;
     const style = {
       width: 128,
       height: 193,
-      backgroundImage: `url(${imgURL})`
+      backgroundImage: `url(${imageLinks.thumbnail})`
     };
+    const bookAuthors =
+      authors !== undefined ? (
+        authors.map((author, i) => (
+          <div key={i} className="book-authors">
+            {author}
+          </div>
+        ))
+      ) : (
+        <div className="book-authors">Unknown</div>
+      );
     return (
       <li>
         <div className="book">
           <div className="book-top">
             <div className="book-cover" style={style} />
             <div className="book-shelf-changer">
-              <Selector currentShelf={shelf} moveShelf={moveShelf} id={id} />
+              <Selector
+                currentShelf={shelf}
+                moveShelf={moveShelf}
+                book={this.props.book}
+              />
             </div>
           </div>
           <div className="book-title">{title}</div>
-          <div className="book-authors">{author}</div>
+          {bookAuthors}
         </div>
       </li>
     );
