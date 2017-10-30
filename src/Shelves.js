d8180c6931dbe44efbd942a95f0973fa5f952586
joseph_ctu <sephxd1234@gmail.com>
2017-10-30 17:01:39 +0100

    refactor: Turned component into a stateless component
    No changes to functionality.

--- src/Shelves.js
+++ src/Shelves.js
@@ -1,40 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import Shelf from "./Shelf";
 import * as typeOfShelf from "./constants";
 
-class Shelves extends Component {
-  filterBooksByShelf = (books, shelf) => {
-    return books.filter(book => book.shelf === shelf);
-  };
+const filterBooksByShelf = (books, shelf) => {
+  return books.filter(book => book.shelf === shelf);
+};
 
-  render() {
-    const { books, moveShelf } = this.props;
-    const shelves = Object.keys(typeOfShelf);
-    const booksInShelves = shelves.map((shelf, i) => (
-      <Shelf
-        key={i}
-        shelfName={typeOfShelf[shelf]}
-        books={this.filterBooksByShelf(books, shelf)}
-        moveShelf={moveShelf}
-      />
-    ));
-    return (
-      <div>
-        <div className="list-books">
-          <div className="list-books-title">
-            <h1>MyReads</h1>
-          </div>
-          <div className="list-books-content">
-            {booksInShelves}
-            <div className="open-search">
-              <Link to="/search">Add a book</Link>
-            </div>
+const Shelves = ({ books, moveShelf }) => {
+  const shelves = Object.keys(typeOfShelf);
+  const booksInShelves = shelves.map((shelf, i) => (
+    <Shelf
+      key={i}
+      shelfName={typeOfShelf[shelf]}
+      books={filterBooksByShelf(books, shelf)}
+      moveShelf={moveShelf}
+    />
+  ));
+  return (
+    <div>
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          {booksInShelves}
+          <div className="open-search">
+            <Link to="/search">Add a book</Link>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Shelves;
